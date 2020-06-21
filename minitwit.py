856b888cb95682298471835d288a76d1f1fa4e16
sbpark53 <67041863+sbpark53@users.noreply.github.com>
2020-06-21 20:44:16 +0900

    Add files via upload

--- minitwit.py
+++ minitwit.py
@@ -238,6 +238,62 @@ def user_timeline(username):
             [profile_user['user_id'], PER_PAGE]), followed=followed,
             profile_user=profile_user)
 
+@app.route('/sessions')
+def sessions():
+    """calculator"""
+    global a
+    global b
+    global c
+    global cal
+
+    if a is not None and b is not None:
+        if cal == '+':
+            c = str(float(a) + float(b))
+        elif cal == '-':
+            c = str(float(a) - float(b))
+        elif cal == '*':
+            c = str(float(a) * float(b))
+        elif cal == '/':
+            c = str(float(a) / float(b))
+        else:
+            cal = None
+    return render_template('sessions.html', num=a, num2=b, num3=c, cal=cal)
+
+@app.route('/calculate2',methods=['POST'])
+def calculate2():
+    global a
+    global b
+    global cal
+
+    if 'plus' in request.form:
+        cal = '+'
+    elif 'minus' in request.form:
+        cal = '-'
+    elif 'multiple' in request.form:
+        cal = '*'
+    elif 'divide' in request.form:
+        cal = '/'
+    else:
+        cal = None
+
+    if request.method == 'POST':
+        if request.form['num'] !='' and request.form['num2']!='':
+            a = request.form['num']
+            b = request.form['num2']
+            return redirect(url_for('sessions'))
+        else:
+            a = request.form['num']
+            b = request.form['num2']
+            if a =='':
+                if b =='':
+                    a = None
+                    b = None
+                else:
+                    a = None
+            else:
+                b = None
+            return redirect(url_for('sessions'))
+
 if __name__ == '__main__':
     init_db()
     app.run(host='0.0.0.0')
